e6347e7e6d3c3fe58a41119a90428aee605af8b3
rajendra <rajendra@ubuntu.lan>
2026-03-12 12:40:20 +0530

    add change db.hs lint issue

--- backend/auth-service/config/db.js
+++ backend/auth-service/config/db.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose")
-require("dotenv").config();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
-    mongoose.connect(process.env.MONGO_URI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
     .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
-}
+    .catch((err) => console.error('MongoDB connection error:', err));
+};
 
-module.exports = connectDB
+module.exports = connectDB;
